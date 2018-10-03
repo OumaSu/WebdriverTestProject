3cea6b7d98a8428dc0dd6a70941931925059c9f9
skaz <nuvern@gmail.com>
2018-10-03 15:33:27 +0500

    folders

---
+++ WebdriverTestProject/WebdriverTestProject/Helpers/ConsoleOutput.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WebdriverTestProject.Helpers
+{
+    public static class ConsoleOutput
+    {
+        public static void WriteConsole(string outputString) => Console.WriteLine(outputString);
+    }
+}
